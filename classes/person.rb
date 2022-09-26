2054a4cccf32635b7b17b80d18d01a04c003e9b6
usbbush <jeanbienvenusb@gmail.com>
2022-09-26 13:19:08 +0200

    Add states and behaviors to Person class

--- classes/person.rb
+++ classes/person.rb
@@ -1,2 +1,23 @@
 class Person
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  attr_reader :id
+
+  attr_accessor :name, :age
+
+  private
+
+  def is_of_age?
+    @age > 18
+  end
+
+  public
+
+  def can_use_services?
+    is_of_age? && @parent_permission
+  end
 end
