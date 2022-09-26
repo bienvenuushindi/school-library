20360eddd16021b6a1ee692272a0b6ad036508b1
usbbush <jeanbienvenusb@gmail.com>
2022-09-26 13:25:36 +0200

    Update Person constructor

--- classes/person.rb
+++ classes/person.rb
@@ -1,5 +1,5 @@
 class Person
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission = true)
     @name = name
     @age = age
     @parent_permission = parent_permission
