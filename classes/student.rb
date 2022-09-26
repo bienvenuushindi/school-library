cf23e161d0257254c7f8df7b2a4f8b6523bc19b5
usbbush <jeanbienvenusb@gmail.com>
2022-09-26 13:26:13 +0200

    Create Student state and methods

--- classes/student.rb
+++ classes/student.rb
@@ -1,2 +1,10 @@
-class Student
+class Student < Person
+  def initialize(classroom, age, name, parent_permission)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+
+  def play_hooky
+    '¯\(ツ)/¯'
+  end
 end
