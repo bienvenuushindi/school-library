1e31ced2f059efdceed8baeebbe4c6155ad164de
usbbush <jeanbienvenusb@gmail.com>
2022-09-26 13:29:50 +0200

    Create Teacher states and methods

--- classes/teacher.rb
+++ classes/teacher.rb
@@ -1,2 +1,10 @@
-class Teacher
+class Teacher < Person
+  def initialize(specialization, age, name, parent_permission)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
 end
