3c4c02f3f1c24e01e4b511a3d07b0baee3c1b888
usbbush <jeanbienvenusb@gmail.com>
2022-10-08 13:12:28 +0200

    Implement BookCreator class

--- ui/creators/book_creator.rb
+++ ui/creators/book_creator.rb
@@ -1,2 +1,21 @@
-class BookCreator
+require_relative '../../association/book'
+class BookCreator < UIReader
+  @instance = new
+  private_instance_methods :new
+  class << self
+    attr_reader :instance
+  end
+  def ask_data
+    print 'Title: '
+    @title = InputReader.read_input
+    print 'Author: '
+    @author = InputReader.read_input
+    self
+  end
+
+  def create
+    book = Book.new(@title, @author)
+    puts 'Book created successfully'
+    book
+  end
 end
