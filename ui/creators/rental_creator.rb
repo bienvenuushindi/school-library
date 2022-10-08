439878c03241bc749d064f9b4e291312071f2b85
usbbush <jeanbienvenusb@gmail.com>
2022-10-08 13:12:56 +0200

    Implement RentalCreator class

--- ui/creators/rental_creator.rb
+++ ui/creators/rental_creator.rb
@@ -1,2 +1,42 @@
-class RentalCreator
+require_relative '../../association/rental'
+class RentalCreator < UIReader
+  @instance = new
+  private_instance_methods :new
+  class << self
+    attr_reader :instance
+  end
+  def ask_data(people, books)
+    ask_book(books).ask_person(people).ask_date
+    self
+  end
+
+  def ask_book(books)
+    puts 'Select a book from the following list by number '
+    books.each_with_index do |el, idx|
+      puts "#{idx}) #{el}"
+    end
+    @book_index = InputReader.read_integer
+    self
+  end
+
+  def ask_person(people)
+    puts 'Select a person from the following list by number (not id)'
+    people.each_with_index do |el, idx|
+      puts "#{idx}) #{el}"
+    end
+    @person_index = InputReader.read_integer
+    self
+  end
+
+  def ask_date
+    print 'Date: '
+    @date = InputReader.read_input
+    self
+  end
+
+  def create(people, books)
+    rental = Rental.new(@date, books[@book_index], people[@person_index])
+    puts 'Rental created successfully'
+    rental
+  end
 end
